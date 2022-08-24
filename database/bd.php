47cc2f91dc84c9e7f45a3147486ff57c4b655040
Lucas Stein <66976832+steinlucas@users.noreply.github.com>
2022-08-23 21:40:36 -0300

    update

--- database/bd.php
+++ database/bd.php
@@ -12,4 +12,29 @@
 
         return $resultado;
     }
+
+    function pesquisarListaLivros() {
+        $conexao = obterConexao();
+        $comandoSQL = "SELECT * FROM livro";
+        $query = mysqli_query($conexao, $comandoSQL);
+        $resultado = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+        return $resultado;
+    }
+
+    function pesquisarEstanteLivros() {
+        $conexao = obterConexao();
+        $comandoSQL = "SELECT L.ID,
+                              L.TITULO,
+		                      A.NOME as nomeAutor,
+		                      E.NOME as nomeEditora
+                        FROM LIVRO_AUTOR LA, LIVRO L, AUTOR A, EDITORA E
+                       WHERE LA.ID_LIVRO = L.ID
+                         AND LA.ID_AUTOR = A.ID
+                         AND L.ID_EDITORA = E.ID;";
+        $query = mysqli_query($conexao, $comandoSQL);
+        $resultado = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+        return $resultado;
+    }
 ?>
---
+++ database/estanteLivros.php
@@ -0,0 +1,46 @@
+<?php
+    include_once("bd.php");
+    $listaLivros = pesquisarEstanteLivros();
+?>
+
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <title>Estante de Livros</title>
+    </head>
+
+    <body>
+    <div class="container">
+    <form method="GET" name="formulario">
+    <h1>Listagem de Livros</h1>
+        <table class="table">
+            <thead>
+                <th scope="col" name="idLivro">Id Livro</th>
+                <th scope="col">Nome Livro</th>
+                <th scope="col">Nome Autor</th>
+                <th scope="col">Nome Editora</th>
+                <th scope="col">Detalhes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($listaLivros as $umLivro) {
+                ?>
+                <tr>
+                    <th scope="row"><?php echo $umLivro['ID'];?></th>
+                    <td><?php echo $umLivro['TITULO'];?></td>
+                    <td><?php echo $umLivro['nomeAutor'];?></td>
+                    <td><?php echo $umLivro['nomeEditora'];?></td>
+                    <td><button type="submit" class="btn btn-primary">+</button></td>
+                <?php
+                    }
+                    echo "GET: ".$_GET['idLivro'].'<br>';
+                ?>
+                </tr>
+            </tbody>
+        </table>
+    </form>
+    </body>
+</html>
