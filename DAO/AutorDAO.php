a75549860212754a0868491081f7e9431da8072c
steinlucas <stein.lucas@icloud.com>
2023-03-22 21:05:09 -0300

    AutorDAO working

--- DAO/AutorDAO.php
+++ DAO/AutorDAO.php
@@ -1,60 +1,28 @@
 <?php
 
+include_once "model\autor.php";
+
 class AutorDAO {
-    function create(){
-        // recebe um objeto e insere no banco.
-    }
-
-    function delete(){
-        // recebe um objeto e dropa do banco.
-    }
-
-    function update(){
-        // recebe um objeto e atualiza no banco.
-    }
 
     function getAutorPorNome($nomeAutor) {
-        $comandoSQL = "SELECT * FROM AUTOR WHERE NOME LIKE ".$nomeAutor."";
-
         $conexao = obterConexao();
+
+        $comandoSQL = "SELECT * FROM AUTOR WHERE NOME LIKE ?";
 
         $stmt = mysqli_prepare($conexao, $comandoSQL);
 
-        $dadosPessoa = mysqli_stmt_get_result($stmt);
-        
-        //$dadosPessoa = $conexao->fetch(PDO::FETCH_ASSOC); #Executa a consulta no banco de dados
+        mysqli_stmt_bind_param($stmt, "s", $nomeAutor);
+        mysqli_stmt_execute($stmt);
 
-        //$query = mysqli_query($conexao, $comandoSQL);
-        //$dadosPessoa = mysqli_fetch_all($query, MYSQLI_ASSOC);
+        $resultado = mysqli_stmt_get_result($stmt);
+        $resultado_array = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 
-        if ($dadosPessoa == false){
-        	return NULL;
-        }
+        //$autor = new Autor();
+        //$autor->setNome($resultado_array['nome']);
+        //$autor->setEmail($resultado_array['email']);
+        //$autor->setWebsite($resultado_array['website']);
 
-        return $dadosPessoa;
-    }
-
-    function getPessoaPorId($id){
-		
-		$sql = "SELECT * FROM pessoa where id = :id";    
-		$stmt = $this->conexao->prepare($sql);
-		$stmt->bindParam(':id', $id);
-		$stmt->execute();
-
-        $dadosPessoa = $stmt->fetch(PDO::FETCH_ASSOC); #Executa a consulta no banco de dados
-        
-        if ($dadosPessoa == false){
-        	return NULL;
-        }
-
-        else{
-        	$pessoa = new Pessoa(); #Cria um objeto pessoa para cada registro do BD
-    	    $pessoa->setId($dadosPessoa['id']);
-    	    $pessoa->setNome($dadosPessoa['nome']);
-    	    $pessoa->setCpf($dadosPessoa['cpf']);
-    	    return $pessoa;
-
-    	}
+        return $resultado_array;
     }
 
 }
